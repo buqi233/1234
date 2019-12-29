338479664fcccaa20b7223ce90a6143cc68b55a5
gitdemo <long331456@163.com>
2019-12-29 20:07:35 +0800

    test

---
+++ src/main/java/demo01.java
@@ -0,0 +1,16 @@
+/**
+ * All rights Reserved, Designed By www.info4z.club
+ * <p>title:PACKAGE_NAME</p>
+ * <p>ClassName:demo01</p>
+ * <p>Description:TODO(请用一句话描述这个类的作用)</p>
+ * <p>Compony:Info4z</p>
+ * author:noauthor
+ * date:2019/12/29
+ * version:1.0
+ * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111");
+    }
+}
